74949d9250b7aa3d83a35e2ce41d90d2be1ea669
workingwithali <alirashid2020e@gmail.com>
2026-02-07 10:45:11 +0500

    add user and auth file

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, NotImplementedException, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+    @HttpCode(HttpStatus.OK)
+    @Post('login')
+    login(@Body() authInput: {username: string; password: string;}) {
+        return this.authService.authenticate(authInput);
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+type AuthInput = {
+    username: string;
+    password: string;
+};
+type SignInData = {
+    userId: number;
+    username: string;
+};
+type authResponse = {
+    accessToken: string;
+    userId: number;
+    userName: string;
+};
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService
+    ) { }
+    async authenticate(authInput: AuthInput): Promise<authResponse> {
+        const user = await this.validateUser(authInput);
+        if (!user) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+        return this.signIn(user);
+    }
+    async validateUser(authInput: AuthInput): Promise<SignInData | null> {
+        const user = await this.usersService.findOne(authInput.username);
+        if (user.password !== authInput.password) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+
+        return {
+            userId: user.userId,
+            username: user.username
+        };
+    }
+    async signIn(user: SignInData): Promise<authResponse> {
+        const playload = { username: user.username, sub: user.userId };
+        const accessToken = await this.jwtService.signAsync(playload);
+        return { accessToken, userId: user.userId, userName: user.username };
+    }
+}
